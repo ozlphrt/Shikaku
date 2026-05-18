2283358f6d961ea3f3f135a375ff6b22d84a5b2b
ozlphrt <ozalph@gmail.com>
2026-05-18 19:37:11 +0300

    Release v2.9.8: Refine text-shadow outlines to a delicate, sharp 1px thickness for perfect typographic definition.

--- src/store/gameStore.js
+++ src/store/gameStore.js
@@ -295,7 +295,7 @@ export const useGameStore = create((set, get) => ({
   starsEarned: loadStarsFromStorage(),
   moveCount: 0,
   lastScoreData: null,
-  appVersion: '2.9.7',
+  appVersion: '2.9.8',
   updateAvailable: false,
   colorByNumber: loadColorByNumberFromStorage(),
 
