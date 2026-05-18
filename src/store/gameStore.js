dbf571eec562cd2e7ded412fc6f087cff4a34545
ozlphrt <ozalph@gmail.com>
2026-05-18 19:25:49 +0300

    Release v2.9.4: Scale down number sizes slightly to a perfect middle-ground value (clamp maximum of 32px) for an exceptionally clean board alignment.

--- src/store/gameStore.js
+++ src/store/gameStore.js
@@ -295,7 +295,7 @@ export const useGameStore = create((set, get) => ({
   starsEarned: loadStarsFromStorage(),
   moveCount: 0,
   lastScoreData: null,
-  appVersion: '2.9.3',
+  appVersion: '2.9.4',
   updateAvailable: false,
   colorByNumber: loadColorByNumberFromStorage(),
 
